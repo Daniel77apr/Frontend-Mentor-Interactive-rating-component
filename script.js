1319f107d177a9eeca3ff79f28313177c69aba06
Daniel77apr <daniel95apr@gmail.com>
2022-10-18 18:21:13 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,59 @@
+// Elements
+const headerEl = document.getElementById("header");
+
+const submitEl = document.getElementById("submit");
+
+const radioEls = document.getElementsByClassName("radio");
+
+const cardEl = document.getElementById("card");
+const formEl = document.getElementById("form");
+const thankYouNoteEl = document.createElement("p");
+thankYouNoteEl.textContent = "We appreciate you taking the time to give a rating. If you ever need more support, don’t hesitate to get in touch!";
+thankYouNoteEl.className = "paragraph center";
+
+const starContainerEl = document.getElementById("star-container");
+const starIconEl = document.getElementById("star-icon");
+const dynamicTextEl = document.createElement("p");
+dynamicTextEl.style.color = "var(--orange)";
+dynamicTextEl.innerHTML = `You selected <span id="dynamic-number"></span> out of 5`;
+const dynamicNumberEl = document.getElementById("dynamic-number");
+/*dynamicNumberEl.textContent = "hello";*/
+
+const phoneImageEl = document.createElement("img");
+phoneImageEl.src = "images/illustration-thank-you.svg";
+phoneImageEl.className = "phone-img";
+
+
+submitEl.addEventListener("click", e => {e.preventDefault()}) //prevents webpage from reloading when clicking on submit
+
+function dynamicNum() {
+    for (let i = 0; i < radioEls.length; i++) {
+        if (radioEls[i].checked) {
+            return radioEls[i].value;
+        }
+    }
+}
+
+// Callback function
+function onSubmit() {
+    if (radioEls[0].checked || radioEls[1].checked || radioEls[2].checked || radioEls[3].checked || radioEls[4].checked) {
+        headerEl.textContent = "Thank you!";
+        headerEl.className += " center";
+
+        console.log(dynamicNum());
+        console.log(dynamicNumberEl);
+        
+        /*dynamicNumberEl.textContent = `${dynamicNum()}`*/
+        cardEl.replaceChild(thankYouNoteEl, formEl);
+
+        starContainerEl.className = "oval";
+        starContainerEl.replaceChild(dynamicTextEl, starIconEl);
+
+        cardEl.insertBefore(phoneImageEl, starContainerEl);
+
+        cardEl.style.padding = "3rem 2.7rem";
+    }
+}
+
+// Event listener
+submitEl.addEventListener("click", onSubmit);
